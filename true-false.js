f7ea7ae0f0a18e49fb8e2e3147cc266a1443cacf
AR-Raqeeb <arraqeeb628@gmail.com>
2021-02-05 02:59:03 +0600

    null vs undefined

--- true-false.js
+++ true-false.js
@@ -1,13 +1,12 @@
 // False values in JS
 // false
 // 0
-// ''
 // undefined
 //null
 // NaN
 // True values in JS
-// '0', ' ', []
-let name = 0;
+// '0', '', []
+let name = '';
 console.log(name);
 if(name || name == 0){
     console.log("condition is true");
