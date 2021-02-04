bb24f5c9717306b4d187fe75770d4e8f0bac0265
AR-Raqeeb <arraqeeb628@gmail.com>
2021-02-05 03:06:31 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+// double equal (==) checks only value, it tries to convert type to match value
+// triple equal (===) checks both value and type
+
+const first = 1;
+const second = true;
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
