5cfa999e5af9f42f39e1ec45d1433a05b7ffdc78
AR-Raqeeb <arraqeeb628@gmail.com>
2021-02-05 02:51:06 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// False values in JS
+// false
+// 0
+// ''
+// undefined
+//null
+// NaN
+// True values in JS
+// '0', ' ', []
+let name = 0;
+console.log(name);
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
